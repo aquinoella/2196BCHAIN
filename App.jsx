079c0165c6b0e7f7995d39c0e3673260066ba5f8
aquinoella <128794538+aquinoella@users.noreply.github.com>
2024-01-12 15:24:46 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css';
+import Greeting from './components/Arrowfunctions/Reactdemo1';
+import Rainy from './components/Arrowfunctions/React-Demo-2';
+import Rainy from './components/Arrowfunctions/React-Demo-3';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>ELAGAIL AQUINO</h1>
+        <Greeting />
+        <Rainy/>
+        <Rainy/>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Reactdemo1.jsx
@@ -0,0 +1,16 @@
+function Greeting(){
+    return(
+        <h1>Hello! I am a Standard functio.</h1>
+    );
+
+const ArrowGreeting = () => {
+    alert ("You clicked me!");
+}
+return(
+    <button onClick={handleClick}>
+    Click This Button!
+    </button>
+);
+}
+
+export default Greeting;
